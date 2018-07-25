949894110aa472be628338ebc7407fd212d2e6bf
Michelle McGowan <boomchick2n4@gmail.com>
2018-07-25 14:14:15 -0700

    added all things I didn't before because I'm learning to use GitHub

---
+++ BookRegistrationEF23/CustomerDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRegistrationEF23
+{
+    static class CustomerDB
+    {
+        public static List<Customer> GetAllCustomers()
+        {
+            BookRegistrationEntities context = new BookRegistrationEntities();
+            List<Customer> customers = context.Customer.ToList();
+
+            return customers;
+        }
+    }
+}
